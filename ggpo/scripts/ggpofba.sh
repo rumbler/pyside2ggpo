559cda070a8dbcdbf7ee18a138ab7a3f9bdf39b2
Pau Oliva <pau@eslack.org>
2014-10-21 16:10:08 +0200

    fixes for linux single executable

--- ggpo/scripts/ggpofba.sh
+++ ggpo/scripts/ggpofba.sh
@@ -8,36 +8,39 @@
 # For best results run 'winecfg' and check the option to "Emulate a virtual desktop"
 # under the Graphics tab. I've it set to 1152x672 for best full screen aspect ratio.
 
-WINE="$1"
-shift
-FBA="$1"
-shift
-
-if [ ! -x ${WINE} ] || [ ! -e ${FBA} ]; then
+FBA="./ggpofba"
+if [ ! -x ${FBA} ] ; then
+	THIS_SCRIPT_PATH=`readlink -f $0`
+	THIS_SCRIPT_DIR=`dirname ${THIS_SCRIPT_PATH}`
+	FBA="${THIS_SCRIPT_DIR}/ggpofba"
+fi
+if [ ! -x ${FBA} ] ; then
+	echo "Can't find ggpofba"
 	exit 1
 fi
 
+
 if [ ! -x /usr/bin/pulseaudio ] || [ ! -x /usr/bin/pacmd ] || [ ! -x /usr/bin/pactl ]; then
-    ${WINE} ${FBA} ${1+"$@"} &
+	${FBA} ${1+"$@"} &
 	exit 0
 fi
 
 # check if there are multiple instances running
-tot=$(ps ax |grep ggpofba.exe |grep quark |wc -l)
+tot=$(ps ax |grep ggpofba |grep quark |wc -l)
 
 # first instance resets pulseaudio, others don't
 if [ ${tot} -eq 0 ]; then
-    VOL=$(/usr/bin/pacmd dump |grep "^set-sink-volume" |tail -n 1 |awk '{print $3}')
-    echo "-!- resetting pulseaudio"
-    /usr/bin/pulseaudio -k
-    /usr/bin/pulseaudio --start
+	VOL=$(/usr/bin/pacmd dump |grep "^set-sink-volume" |tail -n 1 |awk '{print $3}')
+	echo "-!- resetting pulseaudio"
+	/usr/bin/pulseaudio -k
+	/usr/bin/pulseaudio --start
 fi
 
 echo "-!- starting the real ggpofba"
-${WINE} ${FBA} ${1+"$@"} &
+${FBA} ${1+"$@"} &
 
 if [ ${tot} -eq 0 ]; then
-    sleep 1s
-    echo "-!- restoring volume value"
-    /usr/bin/pactl set-sink-volume 0 ${VOL}
+	sleep 1s
+	echo "-!- restoring volume value"
+	/usr/bin/pactl set-sink-volume 0 ${VOL}
 fi
