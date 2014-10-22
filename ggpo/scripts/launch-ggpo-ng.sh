9043f4092430629a80ba13f26f040a361a0790e3
Pau Oliva <pau@eslack.org>
2014-10-22 02:17:24 +0200

    add launcher script for OSX .app

---
+++ ggpo/scripts/launch-ggpo-ng.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# change to the app's directory
+cd "${0%/*}"
+
+# create symlink to ROMs folder in the user's home
+if [ ! -e "ROMs" ] ; then 
+	mkdir -p ${HOME}/ROMs
+	ln -s ${HOME}/ROMs ROMs
+fi
+
+# launch ggpo-ng
+./ggpo-ng
